6edc03565dc58ad90254689329d2a698179ffe25
Will Cannings <me@willcannings.com>
2010-10-31 23:45:08 +1100

    Switched to MurmurHash2 hash function

--- lru_cache.c
+++ lru_cache.c
@@ -7,12 +7,37 @@
 // ------------------------------------------
 // private functions
 // ------------------------------------------
-// bernstein hash function
-lru_cache_item *lru_cache_key_to_item(lru_cache *cache, void *key, uint32_t length) {
-  uint32_t hash = 5381, i = 0;
-  for(; i < length; i++)
-    hash = ((hash << 5) + hash + ((char *)key)[i]);
-  return cache->items[hash % cache->hash_table_size];
+// MurmurHash2, by Austin Appleby
+// http://sites.google.com/site/murmurhash/
+uint32_t lru_cache_key_to_hash(lru_cache *cache, void *key, uint32_t key_length) {
+	uint32_t m = 0x5bd1e995;
+	uint32_t r = 24;
+	uint32_t h = cache->access_count ^ key_length;
+	char* data = (char *)key;
+
+	while(key_length >= 4) {
+    uint32_t k = *(uint32_t *)data;
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+		h *= m;
+		h ^= k;
+		data += 4;
+		key_length -= 4;
+	}
+	
+	switch(key_length) {
+  	case 3: h ^= data[2] << 16;
+  	case 2: h ^= data[1] << 8;
+  	case 1: h ^= data[0];
+  	        h *= m;
+	};
+
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+
+	return h;
 }
 
 // compare a key against an existing item's key
@@ -24,6 +49,23 @@ int lru_cache_cmp_keys(lru_cache_item *item, void *key, uint32_t key_length) {
   }
 }
 
+// error helpers
+#define error_for(conditions, error)  if(conditions) {return error;}
+#define test_for_missing_cache()      error_for(!cache, LRU_CACHE_MISSING_CACHE)
+#define test_for_missing_key()        error_for(!key || key_length == 0, LRU_CACHE_MISSING_KEY)
+#define test_for_missing_value()      error_for(!value || value_length == 0, LRU_CACHE_MISSING_VALUE)
+#define test_for_value_too_large()    error_for(value_length > cache->total_memory, LRU_CACHE_VALUE_TOO_LARGE)
+
+// lock helpers
+#define lock_cache()    if(pthread_mutex_lock(cache->mutex)) {\
+  perror("LRU Cache unable to obtain mutex lock");\
+  return LRU_CACHE_PTHREAD_ERROR;\
+}
+
+#define unlock_cache()  if(pthread_mutex_unlock(cache->mutex)) {\
+  perror("LRU Cache unable to release mutex lock");\
+  return LRU_CACHE_PTHREAD_ERROR;\
+}
 
 // ------------------------------------------
 // public api
@@ -35,8 +77,9 @@ lru_cache *lru_cache_new(uint64_t max_size, uint32_t average_size) {
     perror("LRU Cache unable to create cache object");
     return NULL;
   }
-  cache->free_memory = max_size;
-  cache->hash_table_size = max_size / average_size;
+  cache->hash_table_size  = max_size / average_size;
+  cache->free_memory      = max_size;
+  cache->total_memory     = max_size;
   
   // size the hash table to a guestimate of the number of slots required (assuming a perfect hash)
   cache->items = (lru_cache_item **) calloc(sizeof(lru_cache_item *), cache->hash_table_size);
@@ -58,11 +101,8 @@ lru_cache *lru_cache_new(uint64_t max_size, uint32_t average_size) {
 }
 
 
-void lru_cache_free(lru_cache *cache) {
-  // ensure we have a cache to free
-  if(!cache) {
-    return;
-  }
+lru_cache_error lru_cache_free(lru_cache *cache) {
+  test_for_missing_cache();
   
   // free each of the cached items, and the hash table
   lru_cache_item *item = NULL, *next = NULL;
@@ -71,7 +111,7 @@ void lru_cache_free(lru_cache *cache) {
     for(; i < cache->hash_table_size; i++) {
       item = cache->items[i];    
       while(item) {
-        next = item->next;
+        next = (lru_cache_item *) item->next;
         free(item);
         item = next;
       }
@@ -83,34 +123,43 @@ void lru_cache_free(lru_cache *cache) {
   if(cache->mutex) {
     if(pthread_mutex_destroy(cache->mutex)) {
       perror("LRU Cache unable to destroy mutex");
+      return LRU_CACHE_PTHREAD_ERROR;
     }
   }
   free(cache);
+  
+  return LRU_CACHE_NO_ERROR;
 }
 
 
 lru_cache_error lru_cache_set(lru_cache *cache, void *key, uint32_t key_length, void *value, uint32_t value_length) {
+  test_for_missing_cache();
+  test_for_missing_key();
+  test_for_missing_value();
+  test_for_value_too_large();
+  lock_cache();
   
+  // if we don't have enough space available for this value, remove the oldest items until we do
+  while(cache->free_memory < value_length) {
+    
+  }
+  
+  unlock_cache();
+  return LRU_CACHE_NO_ERROR;
 }
 
 
 lru_cache_error lru_cache_get(lru_cache *cache, void *key, uint32_t key_length, void **value) {
-  // preconditions
-  if(!cache)
-    return MISSING_CACHE;
-  if(!key || key_length == 0)
-    return MISSING_KEY;
-
-  // lock access to the cache
-  if(pthread_mutex_lock(cache->mutex)) {
-    perror("LRU Cache unable to obtain mutex lock");
-    return PTHREAD_ERROR;
-  }
+  test_for_missing_cache();
+  test_for_missing_key();
+  lock_cache();
   
-  // loop until we find the item, or hit the end of the chain
-  lru_cache_item *item = lru_cache_key_to_item(cache, key, key_length);
-  while(item && !lru_cache_cmp_keys(item, key, key_length)) {
-    item = item->next;
+  // loop until we find the item, or hit the end of a chain
+  uint32_t hash = lru_cache_key_to_hash(cache, key, key_length);
+  lru_cache_item *item = cache->items[hash % cache->hash_table_size];
+  
+  while(item && lru_cache_cmp_keys(item, key, key_length)) {
+    item = (lru_cache_item *) item->next;
   }
   
   // if we found the item, update the access counters
@@ -121,15 +170,26 @@ lru_cache_error lru_cache_get(lru_cache *cache, void *key, uint32_t key_length, 
     *value = NULL;
   }
   
-  // release access to the cache
-  if(pthread_mutex_unlock(cache->mutex)) {
-    perror("LRU Cache unable to release mutex lock");
-    return PTHREAD_ERROR;
-  }
-  return NO_ERROR;
+  unlock_cache();
+  return LRU_CACHE_NO_ERROR;
 }
 
 
 lru_cache_error lru_cache_delete(lru_cache *cache, void *key, uint32_t key_length) {
+  test_for_missing_cache();
+  test_for_missing_key();
+  lock_cache();
   
+  // loop until we find the item, or hit the end of a chain
+  lru_cache_item *item = NULL, *prev = NULL;
+  uint32_t hash = lru_cache_key_to_hash(cache, key, key_length);
+  item = cache->items[hash % cache->hash_table_size];
+  
+  while(item && lru_cache_cmp_keys(item, key, key_length)) {
+    prev = item;
+    item = (lru_cache_item *) item->next;
+  }
+  
+  unlock_cache();
+  return LRU_CACHE_NO_ERROR;
 }
--- lru_cache.h
+++ lru_cache.h
@@ -4,14 +4,22 @@
 #ifndef __lru_cache__
 #define __lru_cache__
 
+// ------------------------------------------
+// errors
+// ------------------------------------------
 typedef enum {
-  NO_ERROR = 0,
-  MISSING_CACHE,
-  MISSING_KEY,
-  MISSING_VALUE,
-  PTHREAD_ERROR
+  LRU_CACHE_NO_ERROR = 0,
+  LRU_CACHE_MISSING_CACHE,
+  LRU_CACHE_MISSING_KEY,
+  LRU_CACHE_MISSING_VALUE,
+  LRU_CACHE_PTHREAD_ERROR,
+  LRU_CACHE_VALUE_TOO_LARGE
 } lru_cache_error;
 
+
+// ------------------------------------------
+// types
+// ------------------------------------------
 typedef struct {
   void      *value;
   void      *key;
@@ -25,8 +33,19 @@ typedef struct {
   lru_cache_item  **items;
   uint64_t        access_count;
   uint64_t        free_memory;
+  uint64_t        total_memory;
   uint32_t        hash_table_size;
   pthread_mutex_t *mutex;
 } lru_cache;
+
+
+// ------------------------------------------
+// api
+// ------------------------------------------
+lru_cache *lru_cache_new(uint64_t max_size, uint32_t average_size);
+lru_cache_error lru_cache_free(lru_cache *cache);
+lru_cache_error lru_cache_set(lru_cache *cache, void *key, uint32_t key_length, void *value, uint32_t value_length);
+lru_cache_error lru_cache_get(lru_cache *cache, void *key, uint32_t key_length, void **value);
+lru_cache_error lru_cache_delete(lru_cache *cache, void *key, uint32_t key_length);
 
 #endif
