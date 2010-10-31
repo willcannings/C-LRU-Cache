7047adc021ab8d1aeec2749836f9a11529a4def0
Will Cannings <me@willcannings.com>
2010-11-01 01:23:25 +1100

    Example program showin usage, and Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+example: example.c lru_cache.c lru_cache.h
+	gcc example.c lru_cache.c -o example
---
+++ example.c
@@ -0,0 +1,56 @@
+#include "lru_cache.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <err.h>
+
+#define CACHE_SIZE  (8 * 1024)  // 8k
+#define AVG_SIZE    (2 * 1024)  // 2k
+
+int main(void) {
+  int error = 0;
+  char *get = NULL;
+  
+  // we need to malloc these values because they will be free'd when they're deleted
+  char *key = (char *) malloc(5);
+  strcpy(key, "test");  
+  char *val = (char *) malloc(6);
+  strcpy(val, "value");
+  
+  // create a new cache
+  printf("Creating a cache\n");
+  lru_cache *cache = lru_cache_new(CACHE_SIZE, AVG_SIZE);
+  
+  printf("Setting a value\n");
+  if(error = lru_cache_set(cache, key, 5, val, 6))
+    errx(1, "Error in set: %i\n", error);
+  
+  // get
+  printf("Getting a value\n");
+  if(error = lru_cache_get(cache, "test", 5, (void **)(&get)))
+    errx(1, "Error in get: %i\n", error);
+  
+  if(get)
+    printf("Value returned was: %s\n", get);
+  else
+    printf("No value was returned\n");
+  
+  // delete
+  printf("Deleting a value\n");
+  if(error = lru_cache_delete(cache, key, 5))
+    errx(1, "Error in get: %i\n", error);
+    
+  // get again
+  printf("Getting a value\n");
+  if(error = lru_cache_get(cache, "test", 5, (void **)(&get)))
+    errx(1, "Error in get: %i\n", error);
+  
+  if(get)
+    printf("Value returned was: %s\n", get);
+  else
+    printf("No value was returned\n");
+  
+  printf("Freeing the cache\n");
+  if(error = lru_cache_free(cache))
+    errx(1, "Error in free: %i\n", error);
+}
